bb9097df8082b64534c8b59fecbb0317eb598888
steve macharia <40029452+stevemacharia@users.noreply.github.com>
2023-11-28 18:51:46 +0300

    NAVBAR ADD

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
+import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
-    <h1 className='text-teal-500'>ARIESHELBY</h1>
+    <>
+    <Hero />
+    
+    </>
   )
 }
