a10581295a00f828efec0266629d8ece27174d58
stevemacharia <stevemachariah@outlook.com>
2023-12-06 22:15:30 +0300

    INTRO TO SERVICES

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Hero from '@/components/Hero'
 import Aboutus from '@/components/Aboutus'
+import Services from '@/components/Services'
 
 export default function Home() {
   return (
     <>
     <Aboutus/>
+    <Services/>
     
     </>
   )
---
+++ components/Services.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div className="relative isolate overflow-hidden bg-white px-6 py-16 sm:py-12 lg:overflow-visible lg:px-0">
+        <h2 className='text-5xl text-as-green text-center font-bold pt-12'>Services</h2>
+    </div>
+  )
+}
+
+export default Services
