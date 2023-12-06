5a1415ddea5f826b9a695c36fe902f1099d6aa21
steve macharia <40029452+stevemacharia@users.noreply.github.com>
2023-12-06 18:51:39 +0300

    ABOOUT US SECTION UI DESIGN

--- components/Aboutus.tsx
+++ components/Aboutus.tsx
@@ -5,16 +5,16 @@ const Aboutus = () => {
   return (
 
     <div className="relative isolate overflow-hidden bg-white px-6 py-16 sm:py-12 lg:overflow-visible lg:px-0">
-    <h2 className='text-4xl hover:font-bold text-as-green text-center font-bold py-12'>About Us</h2>
+    <h2 className='text-5xl text-as-green text-center font-bold pt-12'>About Us</h2>
+    <p className="text-sm leading-8 text-gray-400 text-center font-medium italic pb-9">Tailored Tech, Crafted Design. Crafting Solutions as Unique as You</p>
     <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
       <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
         <div className="lg:pr-4">
           <div className="lg:max-w-lg">
-            <p className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</p>
-            <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</h1>
-            <p className="mt-6 text-xl leading-8 text-gray-700">
-              Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam
-              eget aliquam. Quisque id at vitae feugiat egestas.
+            <p className="text-base font-semibold leading-7 hover:font-bold text-yellow2">Background</p>
+            <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Defining Innovation, Designing Excellence</h1>
+            <p className="mt-6 text-base leading-7 text-gray-700">
+            Welcome to Arieshelby, your go-to destination for personalized tech and design solutions. At Arieshelby, we specialize in creating tailor-made solutions to elevate your digital experience. Our team combines expertise in technology and design to bring your unique vision to life. Discover the perfect blend of innovation and customization with Arieshelby – your partner for bespoke solutions that stand out. Explore the possibilities and unlock tailored excellence with us.
             </p>
           </div>
         </div>
