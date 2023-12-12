8451cd25eeda93bdab2c4f7f25d22a36c111558a
stevemacharia <stevemachariah@outlook.com>
2023-12-12 20:52:11 +0300

    SERVICE SECTION NEW UI

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -9,37 +9,31 @@ import {
     Tooltip,
   } from "@material-tailwind/react";
    
-  const products = [
+
+
+const callouts = [
     {
-      id: 1,
-      name: 'Basic Tee',
+      name: 'Desk and Office',
+      description: 'Work from home accessories',
+      imageSrc: '/as_photography.jpg',
+      imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
       href: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      price: '$35',
-      color: 'Black',
     },
     {
-        id: 1,
-        name: 'Basic Tee',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-        imageAlt: "Front of men's Basic Tee in black.",
-        price: '$35',
-        color: 'Black',
-      },
-      {
-        id: 1,
-        name: 'Basic Tee',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-        imageAlt: "Front of men's Basic Tee in black.",
-        price: '$35',
-        color: 'Black',
-      },
-    // More products...
+      name: 'Self-Improvement',
+      description: 'Journals and note-taking',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
+      imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+      href: '#',
+    },
+    {
+      name: 'Travel',
+      description: 'Daily commute essentials',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+      imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+      href: '#',
+    },
   ]
-
 
 
 
@@ -47,46 +41,28 @@ const ServiceCard = () => {
   return (
       <>
     <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4  lg:max-w-7xl lg:px-8">
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            
-            <div key={product.id} className="group relative">
- 
-
-
-              <div className="max-w-sm rounded-lg overflow-hidden shadow-lg">
-                  <img className="w-full" src="/arieshelby_mockup.png" alt="Sunset in the mountains"/>
-                  <div className="px-6 py-4">
-                      <div className="font-bold text-xl mb-2">{product.name}</div>
-                      <p className="text-gray-700 text-base">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                      </p>
-                  </div>
-                  <div className="px-6 pt-4 pb-2">
-                      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{product.color}</span>
-                      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{product.price}</span>
-                      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                  </div>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:max-w-none">
+          <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+            {callouts.map((callout) => (
+              <div key={callout.name} className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <img
+                    src={callout.imageSrc}
+                    alt={callout.imageAlt}
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href={callout.href}>
+                    <span className="absolute inset-0" />
+                    {callout.name}
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">{callout.description}</p>
               </div>
-
-
-
-
-            </div>
-
-
-                
-
-
-
-
-          ))}
-
-
-
-
-
+            ))}
+          </div>
         </div>
       </div>
 
--- components/Services.tsx
+++ components/Services.tsx
@@ -7,7 +7,7 @@ const Services = () => {
     <div className="relative isolate overflow-hidden bg-white px-6 py-16 sm:py-12 lg:overflow-visible lg:px-0">
         <div className="container mx-auto px-8">
           <h2 className='md:text-5xl text-3xl text-as-green text-center font-bold pt-12'>Our Services</h2>
-          <p className="text-sm leading-8 text-gray-400 text-center font-medium italic pb-9">What we offer</p>
+          <p className="text-sm leading-8 text-gray-400 text-center font-medium italic pb-2">What we offer</p>
           <ServiceCard/>
         </div>
     </div>
